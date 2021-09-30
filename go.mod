7f3356399ac60a91b70b7aea97e0cb8b1b8e434a
omigusty <halogusty@gmail.com>
2021-09-30 18:34:18 +0700

    update say hello paramater name

--- go.mod
+++ go.mod
@@ -1,3 +1,3 @@
-module github.com/omigusty/go-say-hello
+module github.com/omigusty/go-say-hello/v2
 
 go 1.17
--- say_hello.go
+++ say_hello.go
@@ -1,5 +1,5 @@
 package gosayhello
 
-func SayHello() string {
-	return "Hello, world!"
+func SayHello(name string) string {
+	return "Hello " + name
 }
